282ca74a8d982f7a2a07f3b23e967911e8fb7a62
arachchigechalani <arachchigechalani@gmail.com>
2022-08-28 14:54:00 +0530

    updated

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express()
 const port = 8000
 
 const user=require('./routes/user')
+const session=require('./routes/session')
 const post=require('./routes/post')
-
+const auth=require('./middleware/auth')
 
 const url='mongodb://localhost/facebook'
 mongoose.connect(url,{useNewUrlParser : true})
@@ -19,7 +20,8 @@ con.on("open",()=>{
 
 
 app.use(express.json())
-app.use('/user',user)
+app.use('/user',auth,user)
+app.use('/session',session)
 app.use('/post',post)
 
 
--- routes/user.js
+++ routes/user.js
@@ -11,12 +11,6 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.post('/login',async(req,res)=>{
-    const response= await User.findOne({email : req.body.email , password : req.body.password});
-    response!=null ? res.json({code:'200',message:'login  successfull',data:response.surname}) : 
-                    res.json({code:'500',message:'login faild',data:null})
- 
- })
 
 router.put('/updateProfile/:email' ,async(req,res)=>{
  const response = await User.findOneAndUpdate({email : req.params.email} , req.body)
@@ -26,32 +20,4 @@ router.put('/updateProfile/:email' ,async(req,res)=>{
 
 })
 
-router.post('/createAccount', async (req, res) => {
-  try {
-      const re = await User.findOne({email: req.body.email});
-      if (re == null) {
-          const user = new User({
-              firstName: req.body.firstName,
-              surName: req.body.surName,
-              gender: req.body.gender,
-              dateOfBirth: req.body.dateOfBirth,
-              phoneNumber: req.body.phoneNumber,
-              email: req.body.email,
-              password: req.body.password
-          })
-          const response = await user.save();
-          response != null ? res.json({code: '200', message: 'Account create successful', data: null}) :
-          res.json({code: '500', message: 'User Account Create Fail', data: null});
-      } else {
-          res.json({code: '500', message: 'Email is Already Exists', data: null});
-      }
-  } catch (err) {
-      res.send('Err : ' + err)
-  }
-})
-
-
-router.post('/login',(req,res)=>{
-    res.send('login account')
-})
 module.exports=router
